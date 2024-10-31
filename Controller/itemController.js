a810fa3e66d8e9ce53fbd6cf712d6b4357094aef
Paboda Sanduni <sandunikhitigama@gmail.com>
2024-10-31 19:19:51 +0530

    update

--- Controller/itemController.js
+++ Controller/itemController.js
@@ -1,5 +1,5 @@
 // Import item model
-import ItemModel from "../model/itemModel";
+import ItemModel from "../model/itemModel.js";
 
 let item_arr = [];
 let selectItemIndex = null;
@@ -34,3 +34,148 @@ const loadItemTable = () => {
 
     });
 };
+
+
+//add button click event
+$('#item_add').on('click',function () {
+    let item_name = $("#itemName").val();
+    let item_price = $("#itemPrice1").val();
+    let item_qty = $("#itemQty").val();
+
+    console.log(item_name,item_price,item_qty);
+
+    if (item_name.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Fill name!",
+        });
+    }else if (item_price.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "fill price!",
+        });
+    }else if (item_qty.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "fill qty!",
+        });
+    }else{
+        const Item = new ItemModel(
+            item_arr.length + 1,
+            item_name,
+            item_price,
+            item_qty
+        );
+
+        item_arr.push(Item);
+        console.log(item_arr);
+        loadItemTable();
+
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        cleanItemFrom();
+    }
+});
+
+// Update item button
+$('#item_update').on('click',function () {
+    let item_name = $("#itemName").val();
+    let item_price = $("#itemPrice1").val();
+    let item_qty = $("#itemQty").val();
+
+
+    if (item_name.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Fill name!",
+        });
+    }else if (item_price.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "fill price!",
+        });
+    }else if (item_qty.length === 0){
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "fill qty!",
+        });
+    }else{
+        const Item2 = new ItemModel(
+            item_arr[selectItemIndex].id,
+            item_name,
+            item_price,
+            item_qty
+        );
+
+        item_arr[selectItemIndex] = Item2;
+        loadItemTable();
+
+        Swal.fire({
+            position: "top-end",
+            icon: "success",
+            title: "Your work has been saved",
+            showConfirmButton: false,
+            timer: 1500
+        });
+        cleanItemFrom();
+    }
+});
+
+
+//delete customer button
+$('#item_delete').on('click',function () {
+    item_arr.splice(selectItemIndex,1);
+    const swalWithBootstrapButtons = Swal.mixin({
+        customClass: {
+            confirmButton: "btn btn-success",
+            cancelButton: "btn btn-danger"
+        },
+        buttonsStyling: false
+    });
+    swalWithBootstrapButtons.fire({
+        title: "Are you sure?",
+        text: "You won't be able to revert this!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Yes, delete it!",
+        cancelButtonText: "No, cancel!",
+        reverseButtons: true
+    }).then((result) => {
+        if (result.isConfirmed) {
+            swalWithBootstrapButtons.fire({
+                title: "Deleted!",
+                text: "Your file has been deleted.",
+                icon: "success"
+            });
+        } else if (
+            /* Read more about handling dismissals below */
+            result.dismiss === Swal.DismissReason.cancel
+        ) {
+            swalWithBootstrapButtons.fire({
+                title: "Cancelled",
+                text: "Your imaginary file is safe :)",
+                icon: "error"
+            });
+        }
+    });
+    cleanItemFrom();
+
+    loadItemTable();
+});
+
+
+
+
+
+
